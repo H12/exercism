6345fccf443e2366615c394babcc0ed2c2f616ec
Henry Firth <henrynf12@gmail.com>
2020-11-04 07:52:17 -0600

    Add initial solution for elixir/markdown

--- elixir/markdown/lib/markdown.ex
+++ elixir/markdown/lib/markdown.ex
@@ -11,69 +11,49 @@ defmodule Markdown do
     "<h1>Header!</h1><ul><li><em>Bold Item</em></li><li><i>Italic Item</i></li></ul>"
   """
   @spec parse(String.t()) :: String.t()
-  def parse(m) do
-    patch(Enum.join(Enum.map(String.split(m, "\n"), fn t -> process(t) end)))
+  def parse(markdown_string) do
+    markdown_string
+    |> String.split("\n")
+    |> Enum.map(&parse_line(&1))
+    |> Enum.join()
+    |> wrap_list_elements
   end
 
-  defp process(t) do
-    if String.starts_with?(t, "#") || String.starts_with?(t, "*") do
-      if String.starts_with?(t, "#") do
-        enclose_with_header_tag(parse_header_md_level(t))
-      else
-        parse_list_md_level(t)
-      end
-    else
-      enclose_with_paragraph_tag(String.split(t))
-    end
+  defp parse_line(line) do
+    line
+    |> String.split()
+    |> parse_words()
+    |> wrap_content_with_tag()
   end
 
-  defp parse_header_md_level(hwt) do
-    [h | t] = String.split(hwt)
-    {to_string(String.length(h)), Enum.join(t, " ")}
+  defp wrap_content_with_tag({tag, content}), do: "<#{tag}>#{content}</#{tag}>"
+
+  defp parse_words(["*" | content]), do: {"li", join_words_with_tags(content)}
+  defp parse_words(["#" | content]), do: {"h1", join_words_with_tags(content)}
+  defp parse_words(["##" | content]), do: {"h2", join_words_with_tags(content)}
+  defp parse_words(["###" | content]), do: {"h3", join_words_with_tags(content)}
+  defp parse_words(["####" | content]), do: {"h4", join_words_with_tags(content)}
+  defp parse_words(["#####" | content]), do: {"h5", join_words_with_tags(content)}
+  defp parse_words(["######" | content]), do: {"h6", join_words_with_tags(content)}
+  defp parse_words(content), do: {"p", join_words_with_tags(content)}
+
+  defp join_words_with_tags(words) do
+    words
+    |> Enum.map(&add_inline_tags(&1))
+    |> Enum.join(" ")
   end
 
-  defp parse_list_md_level(l) do
-    t = String.split(String.trim_leading(l, "* "))
-    "<li>" <> join_words_with_tags(t) <> "</li>"
+  defp add_inline_tags(word) do
+    word
+    |> String.replace_prefix("__", "<strong>")
+    |> String.replace_prefix("_", "<em>")
+    |> String.replace_suffix("__", "</strong>")
+    |> String.replace_suffix("_", "</em>")
   end
 
-  defp enclose_with_header_tag({hl, htl}) do
-    "<h" <> hl <> ">" <> htl <> "</h" <> hl <> ">"
-  end
-
-  defp enclose_with_paragraph_tag(t) do
-    "<p>#{join_words_with_tags(t)}</p>"
-  end
-
-  defp join_words_with_tags(t) do
-    Enum.join(Enum.map(t, fn w -> replace_md_with_tag(w) end), " ")
-  end
-
-  defp replace_md_with_tag(w) do
-    replace_suffix_md(replace_prefix_md(w))
-  end
-
-  defp replace_prefix_md(w) do
-    cond do
-      w =~ ~r/^#{"__"}{1}/ -> String.replace(w, ~r/^#{"__"}{1}/, "<strong>", global: false)
-      w =~ ~r/^[#{"_"}{1}][^#{"_"}+]/ -> String.replace(w, ~r/_/, "<em>", global: false)
-      true -> w
-    end
-  end
-
-  defp replace_suffix_md(w) do
-    cond do
-      w =~ ~r/#{"__"}{1}$/ -> String.replace(w, ~r/#{"__"}{1}$/, "</strong>")
-      w =~ ~r/[^#{"_"}{1}]/ -> String.replace(w, ~r/_/, "</em>")
-      true -> w
-    end
-  end
-
-  defp patch(l) do
-    String.replace_suffix(
-      String.replace(l, "<li>", "<ul>" <> "<li>", global: false),
-      "</li>",
-      "</li>" <> "</ul>"
-    )
+  defp wrap_list_elements(line) do
+    line
+    |> String.replace("<li>", "<ul><li>", global: false)
+    |> String.replace_suffix("</li>", "</li></ul>")
   end
 end
