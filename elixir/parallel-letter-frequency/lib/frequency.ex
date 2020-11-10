997e48c7dd3b9f7d3a70662343905097b3743d1d
Henry Firth <henrynf12@gmail.com>
2020-11-10 17:45:56 -0600

    Add initial solution with workers

--- elixir/parallel-letter-frequency/lib/frequency.ex
+++ elixir/parallel-letter-frequency/lib/frequency.ex
@@ -8,17 +8,38 @@ defmodule Frequency do
   """
   @spec frequency([String.t()], pos_integer) :: map
   def frequency(texts, workers) do
-    # TODO: Figure out workers
-    Enum.reduce(texts, %{}, &accumulate_text/2)
+    combined_text = Enum.reduce(texts, "", &(&1 <> &2))
+    text_length = byte_size(combined_text)
+
+    if text_length == 0 do
+      %{}
+    else
+      divisor = ceil(text_length / workers)
+
+      combined_text
+      |> String.codepoints()
+      |> Enum.chunk_every(divisor)
+      |> Enum.map(&Enum.join/1)
+      |> Enum.map(fn text -> Task.async(fn -> accumulate_text(text) end) end)
+      |> Enum.map(fn task -> Task.await(task) end)
+      |> Enum.reduce(%{}, &merge_frequencies(&2, &1))
+    end
   end
 
-  defp accumulate_text(string, acc) do
+  defp accumulate_text(string) do
     string
     |> String.downcase()
     |> String.trim()
     |> String.split("", trim: true)
     |> Enum.filter(&String.match?(&1, ~r/^[[:lower:]]+$/u))
     |> Enum.frequencies()
-    |> Map.merge(acc, fn _key, new_count, acc_count -> new_count + acc_count end)
+  end
+
+  defp merge_frequencies(frequency, other_frequency) do
+    Map.merge(
+      frequency,
+      other_frequency,
+      fn _key, new_count, acc_count -> new_count + acc_count end
+    )
   end
 end
