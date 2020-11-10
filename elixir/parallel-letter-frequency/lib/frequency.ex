f23ef4d2704dab88a7345d6aba6204e79a1c7332
Henry Firth <henrynf12@gmail.com>
2020-11-10 09:02:11 -0600

    Solve elixir/parallel-letter-frequency w/o workers

--- elixir/parallel-letter-frequency/lib/frequency.ex
+++ elixir/parallel-letter-frequency/lib/frequency.ex
@@ -8,5 +8,17 @@ defmodule Frequency do
   """
   @spec frequency([String.t()], pos_integer) :: map
   def frequency(texts, workers) do
+    # TODO: Figure out workers
+    Enum.reduce(texts, %{}, &accumulate_text/2)
+  end
+
+  defp accumulate_text(string, acc) do
+    string
+    |> String.downcase()
+    |> String.trim()
+    |> String.split("", trim: true)
+    |> Enum.filter(&String.match?(&1, ~r/^[[:lower:]]+$/u))
+    |> Enum.frequencies()
+    |> Map.merge(acc, fn _key, new_count, acc_count -> new_count + acc_count end)
   end
 end
