fe388089940b5c9cf81795b62786c38b6f7c4b63
Henry Firth <henrynf12@gmail.com>
2020-12-08 13:48:23 -0600

    Solve elixir/strain challenge

--- elixir/strain/lib/strain.ex
+++ elixir/strain/lib/strain.ex
@@ -6,7 +6,16 @@ defmodule Strain do
   Do not use `Enum.filter`.
   """
   @spec keep(list :: list(any), fun :: (any -> boolean)) :: list(any)
-  def keep(list, fun) do
+  def keep(list, fun), do: keep(list, fun, [])
+
+  def keep([], _fun, acc), do: Enum.reverse(acc)
+
+  def keep([head | tail], fun, acc) do
+    if fun.(head) do
+      keep(tail, fun, [head | acc])
+    else
+      keep(tail, fun, acc)
+    end
   end
 
   @doc """
@@ -16,6 +25,15 @@ defmodule Strain do
   Do not use `Enum.reject`.
   """
   @spec discard(list :: list(any), fun :: (any -> boolean)) :: list(any)
-  def discard(list, fun) do
+  def discard(list, fun), do: discard(list, fun, [])
+
+  def discard([], _fun, acc), do: Enum.reverse(acc)
+
+  def discard([head | tail], fun, acc) do
+    if fun.(head) do
+      discard(tail, fun, acc)
+    else
+      discard(tail, fun, [head | acc])
+    end
   end
 end
