7f791b591451a79043edf72e3edbe2fde3e58afe
Henry Firth <henrynf12@gmail.com>
2020-12-12 15:00:24 -0600

    Clean up elixir/parallel-letter-frequency challenge

--- elixir/parallel-letter-frequency/lib/frequency.ex
+++ elixir/parallel-letter-frequency/lib/frequency.ex
@@ -7,32 +7,26 @@ defmodule Frequency do
   The number of worker processes to use can be set with 'workers'.
   """
   @spec frequency([String.t()], pos_integer) :: map
+  def frequency(texts, workers)
+
+  def frequency([], _workers), do: %{}
+
   def frequency(texts, workers) do
     combined_text = Enum.reduce(texts, "", &(&1 <> &2))
+
     text_length = byte_size(combined_text)
+    divisor = ceil(text_length / workers)
 
-    if text_length == 0 do
-      %{}
-    else
-      divisor = ceil(text_length / workers)
-
-      combined_text
-      |> String.codepoints()
-      |> Enum.chunk_every(divisor)
-      |> Enum.map(&Enum.join/1)
-      |> Enum.map(fn text -> Task.async(fn -> accumulate_text(text) end) end)
-      |> Enum.map(fn task -> Task.await(task) end)
-      |> Enum.reduce(%{}, &merge_frequencies(&2, &1))
-    end
+    Regex.scan(~r/[[:alpha:]]/u, combined_text)
+    |> List.flatten()
+    |> Enum.chunk_every(divisor)
+    |> Enum.map(&frequency_task/1)
+    |> Enum.map(fn task -> Task.await(task) end)
+    |> Enum.reduce(%{}, &merge_frequencies(&2, &1))
   end
 
-  defp accumulate_text(string) do
-    string
-    |> String.downcase()
-    |> String.trim()
-    |> String.split("", trim: true)
-    |> Enum.filter(&String.match?(&1, ~r/^[[:lower:]]+$/u))
-    |> Enum.frequencies()
+  defp frequency_task(codepoints) do
+    Task.async(fn -> Enum.frequencies_by(codepoints, &String.downcase/1) end)
   end
 
   defp merge_frequencies(frequency, other_frequency) do
