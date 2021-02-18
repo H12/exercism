38f92fc12e125bb1b4b0cdf7d3b2ac3620ed6fe7
Henry Firth <henrynf12@gmail.com>
2021-02-18 07:15:59 -0600

    Initial solution to go/raindrops

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,5 +1,25 @@
 package raindrops
 
+import "strconv"
+
 func Convert(num int) string {
-	return ""
+	output := ""
+
+	if num % 3 == 0 {
+		output += "Pling"
+	}
+
+	if num % 5 == 0 {
+		output += "Plang"
+	}
+
+	if num % 7 == 0 {
+		output += "Plong"
+	}
+
+	if output == "" {
+		return strconv.Itoa(num)
+	}
+
+	return output
 }
