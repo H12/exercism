11e4919e7477c1f9e18a142a43afe77e133aad5a
Henry Firth <henrynf12@gmail.com>
2021-02-27 13:43:56 -0600

    Formatting changes

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,19 +1,22 @@
 package raindrops
 
-import "strconv"
+import (
+	"strconv"
+)
 
+// Convert converts an into into a raindrops string.
 func Convert(num int) string {
 	output := ""
 
-	if num % 3 == 0 {
+	if num%3 == 0 {
 		output += "Pling"
 	}
 
-	if num % 5 == 0 {
+	if num%5 == 0 {
 		output += "Plang"
 	}
 
-	if num % 7 == 0 {
+	if num%7 == 0 {
 		output += "Plong"
 	}
 
