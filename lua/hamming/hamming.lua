720a8c53ad699baaeb5894253d9af7751e35db82
Henry Firth <henrynf12@gmail.com>
2021-08-07 15:14:32 -0500

    Solve lua hamming

--- lua/hamming/hamming.lua
+++ lua/hamming/hamming.lua
@@ -1,7 +1,17 @@
 local Hamming = {}
 
 function Hamming.compute(a,b)
+	if string.len(a) ~= string.len(b) then return -1 end
 
+	local count = 0
+
+	for i=1,string.len(a) do
+		if string.sub(a, i, i) ~= string.sub(b, i, i) then
+			count = count + 1
+		end
+	end
+
+	return count
 end
 
 return Hamming
