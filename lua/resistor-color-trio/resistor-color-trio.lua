edaa8abef108bf6f1f91e1b442d282bc35faf4db
Henry Firth <henrynf12@gmail.com>
2021-08-07 16:38:14 -0500

    Simplify lua/resistor-color-trio

--- lua/resistor-color-trio/resistor-color-trio.lua
+++ lua/resistor-color-trio/resistor-color-trio.lua
@@ -1,30 +1,24 @@
 return {
 	decode = function(c1, c2, c3)
-		local color_to_number = function(color)
-			if color == 'black'  then return 0 end
-			if color == 'brown'  then return 1 end
-			if color == 'red'    then return 2 end
-			if color == 'orange' then return 3 end
-			if color == 'yellow' then return 4 end
-			if color == 'green'  then return 5 end
-			if color == 'blue'   then return 6 end
-			if color == 'violet' then return 7 end
-			if color == 'grey'   then return 8 end
-			if color == 'white'  then return 9 end
+		local color_map = {
+			black  = 0,
+			brown  = 1,
+			red    = 2,
+			orange = 3,
+			yellow = 4,
+			green  = 5,
+			blue   = 6,
+			violet = 7,
+			grey   = 8,
+			white  = 9
+		}
+
+		local ohm_value = (color_map[c1] * 10 + color_map[c2]) * 10^color_map[c3]
+
+		if ohm_value > 1000 then
+			return ohm_value / 1000, "kiloohms"
 		end
 
-		local number_to_ohms = function(num)
-			if num > 1000 then return num / 1000, "kiloohms" end
-
-			return num, "ohms"
-		end
-
-		local n1 = color_to_number(c1)
-		local n2 = color_to_number(c2)
-		local n3 = color_to_number(c3)
-
-		local ohm_value = (n1 * 10 + n2) * (10^n3)
-
-		return number_to_ohms(ohm_value)
+		return ohm_value, "ohms"
 	end,
 }
