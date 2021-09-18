1cbc7e40bf8ea0fc60c546e937723ae0b8656400
Henry Firth <henrynf12@gmail.com>
2021-09-18 16:41:23 -0500

    Solve c/armstrong-numbers

--- c/armstrong-numbers/armstrong_numbers.c
+++ c/armstrong-numbers/armstrong_numbers.c
@@ -1,1 +1,27 @@
+#include <stdlib.h>
+#include <math.h>
 #include "armstrong_numbers.h"
+
+static int calc_armstrong_val(int n)
+{
+	if (n == 0) return 0;
+
+	n = abs(n);
+
+	int len = log10(n) + 1;
+	int sum = 0;
+
+	while (n != 0)
+	{
+		sum += pow(n % 10, len);
+		n /= 10;
+	}
+
+	return sum;
+}
+
+
+bool is_armstrong_number(int n)
+{
+	return n == calc_armstrong_val(n);
+}
--- c/armstrong-numbers/test_armstrong_numbers.c
+++ c/armstrong-numbers/test_armstrong_numbers.c
@@ -16,50 +16,47 @@ static void test_zero_is_an_armstrong_number(void)
 
 static void test_single_digit_numbers_are_armstrong_numbers(void)
 {
-   TEST_IGNORE();               // delete this line to run test
    TEST_ASSERT_TRUE(is_armstrong_number(5));
 }
 
 static void test_there_are_no_two_digit_armstrong_numbers(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_FALSE(is_armstrong_number(10));
 }
 
 static void test_three_digit_number_that_is_an_armstrong_number(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_TRUE(is_armstrong_number(153));
 }
 
 static void test_three_digit_number_that_is_not_an_armstrong_number(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_FALSE(is_armstrong_number(100));
 }
 
 static void test_four_digit_number_that_is_an_armstrong_number(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_TRUE(is_armstrong_number(9474));
 }
 
 static void test_four_digit_number_that_is_not_an_armstrong_number(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_FALSE(is_armstrong_number(9475));
 }
 
 static void test_seven_digit_number_that_is_an_armstrong_number(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_TRUE(is_armstrong_number(9926315));
 }
 
 static void test_seven_digit_number_that_is_not_an_armstrong_number(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_FALSE(is_armstrong_number(9926314));
+}
+
+static void test_negative_number(void)
+{
+   TEST_ASSERT_FALSE(is_armstrong_number(-5));
 }
 
 int main(void)
@@ -75,6 +72,7 @@ int main(void)
    RUN_TEST(test_four_digit_number_that_is_not_an_armstrong_number);
    RUN_TEST(test_seven_digit_number_that_is_an_armstrong_number);
    RUN_TEST(test_seven_digit_number_that_is_not_an_armstrong_number);
+   RUN_TEST(test_negative_number);
 
    return UnityEnd();
 }
