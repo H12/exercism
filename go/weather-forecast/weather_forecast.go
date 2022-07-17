346ed6229ed8f49df829bf74aa07615b140b2e9f
Henry Firth <henrynf12@gmail.com>
2022-07-17 11:00:27 -0500

    Solve go/weather-forecast

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather provides helpful weather-related functions.
 package weather
 
+// CurrentCondition represents the current weather condition.
 var CurrentCondition string
+// CurrentLocation represents the location in which CurrentCondition applies.
 var CurrentLocation string
 
+// Forecast provides a human-readable weather forecast when provided a city and
+// the current weather condition.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
