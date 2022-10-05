4c37eb53737e644f5c9f9fe2bc87699e295df1c8
Henry Firth <henrynf12@gmail.com>
2022-10-04 22:09:21 -0500

    Solve go/lasagna-master

--- go/lasagna-master/lasagna_master.go
+++ go/lasagna-master/lasagna_master.go
@@ -1,9 +1,39 @@
 package lasagna
 
-// TODO: define the 'PreparationTime()' function
+func PreparationTime(layers []string, prepTime int) int {
+	if prepTime == 0 {
+		prepTime = 2
+	}
 
-// TODO: define the 'Quantities()' function
+	return len(layers) * prepTime
+}
 
-// TODO: define the 'AddSecretIngredient()' function
+func Quantities(layers []string) (int, float64) {
+	gramsNoodle := 0
+	litersSauce := 0.0
 
-// TODO: define the 'ScaleRecipe()' function
+	for _, ingredient := range layers {
+		if ingredient == "noodles" {
+			gramsNoodle += 50
+		}
+
+		if ingredient == "sauce" {
+			litersSauce += 0.2
+		}
+	}
+
+	return gramsNoodle, litersSauce
+}
+
+func AddSecretIngredient(specialRecipe, myRecipe []string) {
+	myRecipe[len(myRecipe)-1] = specialRecipe[len(specialRecipe)-1]
+}
+
+func ScaleRecipe(quantities []float64, numPortions int) []float64 {
+	scaledQuantities := make([]float64, len(quantities))
+	for i, amount := range quantities {
+		scaledQuantities[i] = amount * float64(numPortions) / 2.0
+	}
+
+	return scaledQuantities
+}
