f2ba7ac8e6f2aeb2130d1b2f8ead9cb89222dd69
H12 <henrynf12@gmail.com>
2024-05-17 01:30:41 -0500

    Solve rust/clock

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,15 +1,36 @@
-pub struct Clock;
+#[derive(Debug, PartialEq)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        let mut rolled_hours = (hours + minutes / 60) % 24;
+        let mut rolled_minutes = minutes % 60;
+
+        if rolled_minutes < 0 {
+            rolled_hours -= 1;
+            rolled_minutes += 60;
+        }
+
+        if rolled_hours < 0 {
+            rolled_hours += 24;
+        }
+
+        return Clock {
+            hours: rolled_hours,
+            minutes: rolled_minutes,
+        };
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+        Clock::new(self.hours, self.minutes + minutes)
+    }
+}
+
+impl std::fmt::Display for Clock {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
     }
 }
